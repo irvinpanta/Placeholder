f3045ece238bf8d0d915da17e9693f306797c404
irvinpanta <irvinpanta96@gmail.com>
2021-07-12 00:36:20 -0500

    Placeholder

---
+++ Placeholder/Metodos.vb
@@ -0,0 +1,17 @@
+﻿Module Metodos
+
+    Public Sub eventoGotFocus(ByVal e As System.EventArgs, caja As TextBox, texto As String)
+        If caja.Text = texto Then
+            caja.ForeColor = Color.Black
+            caja.Text = ""
+        End If
+    End Sub
+
+    Public Sub eventoLostFocus(ByVal e As System.EventArgs, caja As TextBox, texto As String)
+        If caja.Text = Nothing Then
+            caja.ForeColor = Color.Gray
+            caja.Text = texto
+        End If
+    End Sub
+
+End Module
---
+++ Placeholder/Placeholder.vb
@@ -0,0 +1,25 @@
+﻿Public Class Placeholder
+
+
+    Private Sub Placeholder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtUsuario.Text = "Ingrese Usuario"
+        txtPass.Text = "Ingrese Constraseña"
+    End Sub
+
+
+    Private Sub txtUsuario_GotFocus(sender As Object, e As EventArgs) Handles txtUsuario.GotFocus
+        eventoGotFocus(e, txtUsuario, "Ingrese Usuario")
+    End Sub
+
+    Private Sub txtUsuario_LostFocus(sender As Object, e As EventArgs) Handles txtUsuario.LostFocus
+        eventoLostFocus(e, txtUsuario, "Ingrese Usuario")
+    End Sub
+
+    Private Sub txtPass_GotFocus(sender As Object, e As EventArgs) Handles txtPass.GotFocus
+        eventoGotFocus(e, txtPass, "Ingrese Constraseña")
+    End Sub
+
+    Private Sub txtPass_LostFocus(sender As Object, e As EventArgs) Handles txtPass.LostFocus
+        eventoLostFocus(e, txtPass, "Ingrese Constraseña")
+    End Sub
+End Class
